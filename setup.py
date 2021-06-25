c923f3712df36e43c98586664c228a9c9c98cb80
Roman Esin <esinromanswift@gmail.com>
2021-06-26 00:19:36 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="todo_database-RomanEsin",
+    name="todo_database",
     version="0.0.1",
     author="Roman Esin",
     author_email="esinromanswift@gmail.com",
